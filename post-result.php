98ea4625265b21f4899941a63ea3fd42aca4b58d
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-06 14:15:12 -0500

    method function

--- post-result.php
+++ post-result.php
@@ -5,15 +5,16 @@ include "view-header.php";
     <h1>Post Result</h1>
 
 <?php
-if (isset($_POST['my-name'])) {
-    ?>
-    <p>The value sent is:</p>
-<?php
-    echo $_POST['my-name'];
-} else {
-  ?>
-    <p>Nothing posted to the page.</p>
-<?php
-}
+echo  getDisplay();
 include "view-footer.php";
+function getDisplay(){
+    if (isset($_POST['my-name'])) {
+
+        return "<p>The value sent is:</p>" . $_POST['my-name'];
+    } else {
+
+        return "<p>Nothing posted to the page.</p>";
+
+    }
+}
 ?>
