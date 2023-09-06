530a714a283501e2058ae85e1d25f232412907b6
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-06 14:09:33 -0500

    did getting

---
+++ get-result.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle = "Get Result";
+include "view-header.php";
+?>
+    <h1>Get Result</h1>
+
+<?php
+if (isset($_GET['my-name1'])) {
+    ?>
+    <p>The value sent is:</p>
+    <?php
+    echo $_GET['my-name1'];
+} else {
+    ?>
+    <p>Nothing getted to the page.</p>
+    <?php
+}
+include "view-footer.php";
+?>
---
+++ get.php
@@ -0,0 +1,13 @@
+
+<?php
+$pageTitle = "Get";
+include "view-header.php";
+?>
+    <h1>Get</h1>
+<form method="get" action="get-result.php">
+    <input type="text" name="my-name1">
+    <input type="submit" value="Get Data">
+</form>
+<?php
+include "view-footer.php";
+?>
